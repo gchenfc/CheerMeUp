a2bb664ea8ec40fa7c8ba6bf23839097f5b14f66
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-01-03 07:08:17 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -109,7 +109,7 @@ class CancelOrStopIntentHandler(AbstractRequestHandler):
         # type: (HandlerInput) -> bool
         return (is_intent_name("AMAZON.CancelIntent")(handler_input) or
                 is_intent_name("AMAZON.StopIntent")(handler_input) or
-                is_intent_name("AMAZON.PauseIntent"(handler_input)))
+                is_intent_name("AMAZON.PauseIntent")(handler_input))
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
