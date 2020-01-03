ccaa02e45dc21e69772ebdd7d3015c49306e9bdf
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-01-03 06:20:05 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -40,6 +40,9 @@ class MainHandler(AbstractRequestHandler):
         # random_fact = random.choice(data[prompts.FACTS])
         random_fact = "hello"
         all_messages = data[prompts.AUDIO_BITS]
+        # for message in all_messages:
+        #     if message
+            
         speech = data[prompts.EMOTION_MESSAGE].format(random_fact)
 
         handler_input.response_builder.speak(speech)
